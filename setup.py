fda041fe88c62c000a287fe737ceaf0856b6d662
Zha Yan <zhayan@sjtu.edu.cn>
2019-11-12 15:47:41 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-VEDIO_PATH = sys.argv[1]
+VIDEO_PATH = sys.argv[1]
 
 VIDEO_NAME = str(VIDEO_PATH).split(".")[0]
 
 
-os.system("python3 ./detection/detection.py " + VEDIO_NAME)
+os.system("python3 ./detection/detection.py " + VIDEO_NAME)
 
-os.system("python3 ./tracking/finger-phone.py " + VEDIO_PATH)
+os.system("python3 ./tracking/finger-phone.py " + VIDEO_PATH)
 
-os.system("python3 ./trajectory/simplify.py " + VEDIO_PATH)
+os.system("python3 ./trajectory/simplify.py " + VIDEO_PATH)
